712277562aa1720a8e4139b19953e073f93f8db7
lucazpinheiro <lucascpinheiro10@gmail.com>
2024-02-20 23:20:37 -0300

    Hello, World chapter

--- fundamentals/hello.go
+++ fundamentals/hello.go
@@ -1,11 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello() string {
-	return "Hello, world"
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := greetingPrefix(language)
+
+	return fmt.Sprintf("%s%s", prefix, name)
+}
+
+// this function was created as the book/tutorial suggests, because the Hello() function
+// was getting to big, personally, I don't think this function was necessary, but I will
+// write this to follow along the tutorial
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("lucas", "English"))
 }
--- fundamentals/hello_test.go
+++ fundamentals/hello_test.go
@@ -3,9 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("lucas", "English")
+		want := "Hello, lucas"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Marcel", "French")
+		want := "Bonjour, Marcel"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
