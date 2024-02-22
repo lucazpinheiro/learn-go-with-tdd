d6506d1b88b8d7b1a0c8c8e413d2501b74f726f5
lucazpinheiro <lucascpinheiro10@gmail.com>
2024-02-22 15:28:01 -0300

    iteration chapter

---
+++ fundamentals/iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+// benchmark function, will run the aimed function a N number of times defined by the framework
+// with the b.N value passed as parameter to an for loop
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("x", 6))
+	// Output: xxxxxx
+}
