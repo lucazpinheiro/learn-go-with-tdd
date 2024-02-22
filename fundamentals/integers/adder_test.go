6d1bc20417419a90470687c8c8936fda742043c8
lucazpinheiro <lucascpinheiro10@gmail.com>
2024-02-21 23:27:05 -0300

    Integers chapter

---
+++ fundamentals/integers/adder_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+// exemple code, is a GO feature, it will run with the tests
+// it provides an example of how the function is used
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
