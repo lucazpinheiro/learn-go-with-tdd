4a631d09f4e29b4e968242e29b38826baf31d669
lucazpinheiro <lucascpinheiro10@gmail.com>
2024-03-01 23:28:15 -0300

    mocking chapter wip

---
+++ fundamentals/mocking/countdown.go
@@ -0,0 +1,28 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+}
--- main.go
+++ main.go
@@ -1,3 +1,22 @@
+/*** MOCKING CHAPTER ***/
+package main
+
+import (
+	"os"
+
+	"github.com/lucazpinheiro/learn-go-with-tdd/fundamentals/mocking"
+)
+
+// func Countdown(out io.Writer) {
+// 	fmt.Fprint(out, "3")
+// }
+
+func main() {
+	sleeper := &mocking.DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)
+}
+
+/*** DEPENDENCY INJECTION CHAPTER ***/
 // example of DI using the io.Writer interface to pass
 // a http.ResponseWriter where data will be written.
 
